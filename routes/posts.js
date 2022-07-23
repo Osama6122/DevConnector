e4c190668f3c7c1aa05ef962aa4a3da604b2f255
Osama <osamanaeem6122@gmail.com>
2022-07-23 06:10:28 +0500

    User Schema
    Created a model User

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/posts
+// @route   GET /posts
 // @desc    Test route
 // @access  Public
 router.get('/', (req,res) => res.send('Posts route'));
--- server.js
+++ server.js
@@ -9,10 +9,10 @@ connectDB();
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/users', require('./routes/users'));
+app.use('/auth', require('./routes/auth'));
+app.use('/profile', require('./routes/profile'));
+app.use('/posts', require('./routes/posts'));
 
 const PORT = process.env.PORT || 5000;
 
