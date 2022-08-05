829a4bef6957a220fde2aab34c7833fa66cfbb85
Osama <osamanaeem6122@gmail.com>
2022-08-05 06:05:13 +0500

    React Router Setup
    Also made some syntax changes in index.js since we are using the latest react version which is v18.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => ( 
+  <Router>
     <Fragment>
       <NavBar />
-      <Landing />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+        </Routes>
+        <section className='container'>
+          <Routes>
+          <Route exact path='/Register' element={<Register />} />
+            <Route exact path='/Login' element={<Login />} />
+          </Routes>
+        </section>
+
+
     </Fragment>
+  </Router>
 );
 
 export default App;
