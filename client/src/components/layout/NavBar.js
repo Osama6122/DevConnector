ffdf66eb27b689ef2472e8f62b0c192d08304cef
Osama <osamanaeem6122@gmail.com>
2022-08-12 10:00:19 +0500

    Starting on the Dashboard
    Checks to see if the user has profile setup or not, Added a dashboard on the navbar which can only be accessed by a user who is logged in, also updated the code in order to clear profile info when the user logs out

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -9,11 +9,17 @@ const NavBar = ({ auth: {isAuthenticated, loading }, logout }) => {
 
   const authLinks = (
     <ul>
-    <li>
-      <a onClick={logout} href="#!">
-      <i className="fas fa-sign-out-alt"></i>{' '}
-      <span className='hide-sm'> Logout</span>
-      </a>
+      <li>
+        <Link to="/dashboard">
+        <i className="fas fa-user"></i>{' '}
+        <span className='hide-sm'> Dashboard </span>
+        </Link>
+      </li> 
+      <li>
+        <a onClick={logout} href="#!">
+        <i className="fas fa-sign-out-alt"></i>{' '}
+        <span className='hide-sm'> Logout</span>
+        </a>
       </li>
   </ul>
   );
